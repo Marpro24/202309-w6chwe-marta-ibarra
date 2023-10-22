a3901e7e7a360a38b8c09163083aa4288bc40664
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-10-22 13:40:44 +0200

    Add App class

---
+++ src/components/App/App.ts
@@ -0,0 +1,19 @@
+import Component from "../Component/Component.js";
+
+class App extends Component {
+  constructor(parentElement: Element) {
+    super(parentElement, "div", "container");
+  }
+
+  protected populate(): void {
+    this.element.innerHTML = `
+    <header class="main-header">
+    <h1 class="main-title>Pokemons</h1>
+    </header>
+    <section class="controls></section>
+    <main class="main"></main>
+    `;
+  }
+}
+
+export default App;
