9bc6d8e8758d2c7043eb8adb8aab20c8eb6b0fac
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-10-22 17:27:46 +0200

    Add body const to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import App from "./components/App/App.js";
 import { getPokemonApi } from "./getPokemonApi/getPokemonApi.js";
 
 const urlApi = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
@@ -6,3 +7,7 @@ const pokemonOffset = 20;
 const pokemons = await getPokemonApi(urlApi, pokemonOffset);
 
 export default pokemons;
+
+const bodyElement = document.querySelector("body")!;
+const appElement = new App(bodyElement);
+appElement.render();
